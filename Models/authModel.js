4d2009a80329e0cdd57c2d072b0067da8daeaaad
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-25 12:37:19 +0500

    Create auth model for my project

--- Models/authModel.js
+++ Models/authModel.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+
+const authSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'public'],
+        default: 'public',
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Auth', authSchema);
