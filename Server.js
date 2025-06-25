7b9e77a519bb4f24f7732460cb140106051390ea
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-25 12:17:06 +0500

    Create server for our project

---
+++ Server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+
+
+
+
+
+
+const port = 4000;
+app.listen(port , () => console.log(`Your server is running on post number ${port}`) );
